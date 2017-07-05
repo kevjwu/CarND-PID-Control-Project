880cee13174856b3a019c57c04cad221fb5777ae
kevjwu <kevinwu103@gmail.com>
2017-07-04 23:54:29 -0400

    Solutions, reflection

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,21 +1,28 @@
 #include "PID.h"
 
-using namespace std;
-
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double kp, double ki, double kd) {
+  Kp = kp;
+  Ki = ki;
+  Kd = kd;
+  p_error = 0;
+  d_error = 0;
+  i_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+  if (p_error != 0){
+    d_error = cte - p_error;
+  }
+  p_error = cte;
+  i_error += cte;
+  
 }
 
 double PID::TotalError() {
+  return -(Kp*p_error + Kd*d_error+ Ki*i_error);
 }
 
